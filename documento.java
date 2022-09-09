8e680240e398e0ab2d7ef81d7d9381cd0517c71a
ivanfermin <ivanfermin2@gmail.com>
2022-09-09 10:36:40 -0400

    ExamenINFORMATICA

---
+++ documento.java
@@ -0,0 +1,15 @@
+public class Documento{
+    String titulo;
+    String url;
+    Tema tema;
+    Index index;
+    public class(){
+
+    }
+    public Documento(String url, Tema tema, Index index){
+        this.titulo=titulo;
+        this.url=url;
+        this.tema=tema;
+        this.index=index;
+    }
+}
